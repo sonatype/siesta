5c99af8aef9e5cafd8b36b45375673d5fc9d8e78
Jason Dillon <jason@planet57.com>
2014-04-30 14:11:38 -0700

    Add logging and MDC

--- siesta2-server/src/main/java/org/sonatype/siesta/server/SiestaServlet.java
+++ siesta2-server/src/main/java/org/sonatype/siesta/server/SiestaServlet.java
@@ -1,11 +1,19 @@
 package org.sonatype.siesta.server;
 
+import java.io.IOException;
+
 import javax.inject.Inject;
 import javax.inject.Named;
 import javax.inject.Singleton;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.glassfish.jersey.server.ResourceConfig;
 import org.glassfish.jersey.servlet.ServletContainer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
 
 import static com.google.common.base.Preconditions.checkNotNull;
 
@@ -19,8 +27,36 @@ import static com.google.common.base.Preconditions.checkNotNull;
 public class SiestaServlet
   extends ServletContainer
 {
+  private final Logger log = LoggerFactory.getLogger(getClass());
+
   @Inject
   public SiestaServlet(final ResourceConfig resourceConfig) {
     super(checkNotNull(resourceConfig));
+  }
+
+  @Override
+  public void service(final HttpServletRequest request, final HttpServletResponse response)
+      throws ServletException, IOException
+  {
+    checkNotNull(request);
+    checkNotNull(response);
+
+    // Log the request URI+URL muck
+    String uri = request.getRequestURI();
+    if (request.getQueryString() != null) {
+      uri = String.format("%s?%s", uri, request.getQueryString());
+    }
+
+    if (log.isDebugEnabled()) {
+      log.debug("Processing: {} {} ({})", request.getMethod(), uri, request.getRequestURL());
+    }
+
+    MDC.put(getClass().getName(), uri);
+    try {
+      super.service(request, response);
+    }
+    finally {
+      MDC.remove(getClass().getName());
+    }
   }
 }
