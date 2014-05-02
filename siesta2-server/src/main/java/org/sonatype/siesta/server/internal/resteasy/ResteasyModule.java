571b5d22b8661cea3d7ed2d5cc17ee176a7ad5cd
Jason Dillon <jason@planet57.com>
2014-05-02 12:18:42 -0700

    Add dispatcher provider binding

--- siesta2-server/src/main/java/org/sonatype/siesta/server/internal/resteasy/ResteasyModule.java
+++ siesta2-server/src/main/java/org/sonatype/siesta/server/internal/resteasy/ResteasyModule.java
@@ -5,6 +5,8 @@ import javax.inject.Singleton;
 import org.sonatype.siesta.server.ComponentContainer;
 
 import com.google.inject.AbstractModule;
+import com.google.inject.Provides;
+import org.jboss.resteasy.core.Dispatcher;
 
 /**
  * RESTEasy module.
@@ -17,5 +19,13 @@ public class ResteasyModule
   @Override
   protected void configure() {
     bind(ComponentContainer.class).to(ComponentContainerImpl.class).in(Singleton.class);
+  }
+
+  /**
+   * Expose RESTEasy {@link Dispatcher} binding.
+   */
+  @Provides
+  public Dispatcher dispatcher(final ComponentContainer container) {
+    return ((ComponentContainerImpl)container).getDispatcher();
   }
 }
