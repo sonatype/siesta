7f4e9150fce3ace4a9acd8564310231c1dc9d9b7
Jason Dillon <jason@planet57.com>
2014-05-08 15:43:51 -0700

    seperators for readablity

--- siesta-api/src/main/java/org/sonatype/siesta/MediaTypes.java
+++ siesta-api/src/main/java/org/sonatype/siesta/MediaTypes.java
@@ -21,17 +21,25 @@ import javax.ws.rs.core.MediaType;
  */
 public class MediaTypes
 {
+  // application/vnd.siesta-error-v1+xml
+
   public static final String VND_ERROR_V1_XML = "application/vnd.siesta-error-v1+xml";
 
   public static final MediaType VND_ERROR_V1_XML_TYPE = new MediaType("application", "vnd.siesta-error-v1+xml");
+
+  // application/vnd.siesta-error-v1+xml
 
   public static final String VND_ERROR_V1_JSON = "application/vnd.siesta-error-v1+json";
 
   public static final MediaType VND_ERROR_V1_JSON_TYPE = new MediaType("application", "vnd.siesta-error-v1+json");
 
+  // application/vnd.siesta-validation-errors-v1+xml
+
   public static final String VND_VALIDATION_ERRORS_V1_XML = "application/vnd.siesta-validation-errors-v1+xml";
 
   public static final MediaType VND_VALIDATION_ERRORS_V1_XML_TYPE = new MediaType("application", "vnd.siesta-validation-errors-v1+xml");
+
+  // application/vnd.siesta-validation-errors-v1+json
 
   public static final String VND_VALIDATION_ERRORS_V1_JSON = "application/vnd.siesta-validation-errors-v1+json";
 
